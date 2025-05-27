33e2a782dc10eca924fca6e9005645eb894ca37f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-27 09:13:43 -0300

    primeiro commit

---
+++ src/test/java/sos_cidadao/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sos_cidadao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
